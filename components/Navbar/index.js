960c8eca0f30c6e2369c537e994ed8246fb52b29
NathanSchroter <ndschroter@gmail.com>
2024-05-17 13:53:22 -0700

    adding folders

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,58 +1,35 @@
-import { useCallback } from 'react';
-import Image from 'next/image';
-import styles from "@/components/Navbar/navbar.module.css"
+import styles from "@/components/Navbar/navbar.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-import Link from 'next/link';
-
-
-const NavBar= () => {
-  	
-  	const onProfileContainerClick = useCallback(() => {
-  	}, []);
-  	
-  	
-  	const onEmergencyContainerClick = useCallback(() => {
-  	}, []);
-  	
-  	
-  	const onNotificationContainerClick = useCallback(() => {
-  	}, []);
-  	
-  	
-  	const onMessagesContainerClick = useCallback(() => {
-  	}, []);
-  	
-  	
-  	const onHomeContainerClick = useCallback(() => {
-  	}, []);
-  	
-  	return (
-    		<div className={styles.navbar}>
-      			<div className={styles.navbarChild} />
-      			<div className={styles.navbarItem} />
-				  <div className={styles.home} onClick={onHomeContainerClick}>
-					<Link href={"/homepage"}>
-        				{/* <div className={styles.notifications}>Home</div> */}
+export default function Navbar() {
+return (
+    <>
+    <div className={styles.navbar}>
+                  <div className={styles.navbarChild} />
+                  <div className={styles.navbarItem} />
+                  <div className={styles.home}>
+                    <Link href={"/homepage"}>
+                        <div className={styles.notifications}>Home</div>
                 <Image className={styles.homeIcon} src={"/Images/home.svg"} alt="Home" height={1} width={1}/>
-				</Link>
-      			</div>
-      			<div className={styles.profile} onClick={onProfileContainerClick}>
-					<Link href={"/programs"}>
-        				{/* <div className={styles.notifications}>Talk
-						</div> */}
-			
-                		<Image className={styles.forYouIcon} src={"/Images/for_you.svg"} alt="Profile" height={1} width={1}/>
-					</Link>
-      			</div>
-      			<div className={styles.emergency}>
-						<Link href={"/emergency"}>
-        				{/* <div className={styles.notifications}>Emergency</div> */}
-						<Image className={styles.callIcon} src={"/Images/call.svg"} alt="Emergency" height={1} width={1}/>
-				</Link>
-      			</div>
-      		
-    		</div>
-			);
-};
+                </Link>
+                  </div>
+                  <div className={styles.profile}>
+                    <Link href={"/programs"}>
+                        <div className={styles.notifications}>Talk
+                        </div>
 
-export default NavBar;
+                        <Image className={styles.forYouIcon} src={"/Images/for_you.svg"} alt="Profile" height={1} width={1}/>
+                    </Link>
+                  </div>
+                  <div className={styles.emergency}>
+                        <Link href={"/emergency"}>
+                        <div className={styles.notifications}>Emergency</div>
+                        <Image className={styles.callIcon} src={"/Images/call.svg"} alt="Emergency" height={1} width={1}/>
+                </Link>
+                  </div>
+
+            </div>
+    </>
+)
+}
